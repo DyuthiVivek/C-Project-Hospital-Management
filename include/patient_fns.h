7020db046e7831add1804ce7e39d85a2364b6385
DyuthiVivek <Dyuthi.Vivek@iiitb.ac.in>
2023-03-05 16:26:15 +0530

    Deleted un-necessary files. Moved includes into another folder.

--- include/patient_fns.h
+++ include/patient_fns.h
@@ -5,8 +5,10 @@ typedef struct patient{
 	char gender[2];
 	char e_contact_name[30];
 	char e_contact_phone[12];
-	char history_file[34]; //name of file which contains history of patient's illness
+	char history_file[35]; //name of file which contains history of patient's illness
 	char presc_file[20]; //name of file with list of precription files
 } patient;
+
 void add_patient();
 patient *read_patient(int *no_of_rec);
+int check_patient(char p[30]);
