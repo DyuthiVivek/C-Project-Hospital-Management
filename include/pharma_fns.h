aa44d71b0a247dfbad2b820cd3caf1c5ede40563
PVSSukeerthi <sukeerthipolasanapalli@gmail.com>
2023-03-07 16:29:16 +0530

    final change

--- include/pharma_fns.h
+++ include/pharma_fns.h
@@ -1,8 +1,9 @@
-struct pharma* deduct_medicicne(struct pharma a,int new_stock);
+void deduct_medicine(char name[20]);
 int check_medicines(char med[20]);
 struct pharma add_medicines(struct pharma a);
 int read_medicines();
-struct pharma add_stock(struct pharma a,int new_stock,char name[20]);
+void add_stock(int new_stock,char name[20]);
+struct node* get_med_prescription(struct node*head);
 struct pharma {
 	char name[20];
 	int stock;
